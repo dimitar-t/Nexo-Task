b78f25808c460e1c3a2b295bc00ea1c2edc0e8da
dimitar-t <dimitartachev23@gmail.com>
2022-07-15 11:13:37 +0300

    Use MySQL db in the application
    Additional route that verifies if the app can communicate with the
    database.
    The password for the connection is passed a docker secret in the form of
    a file.

--- app/hello-world.py
+++ app/hello-world.py
@@ -1,9 +1,29 @@
+from urllib.parse import _ResultMixinStr
 from flask import Flask
+import mysql.connector
+import os
+
 
 app = Flask(__name__)
+db = mysql.connector.connect(
+    host="mysql",
+    user="nexo",
+    password=open(os.environ.get("MYSQL_PASSWORD")).read(),
+    database="nexo-db",
+    port="3306"
+)
+
 
 @app.route("/")
 def index():
     return "Hello, Nexo!"
 
-app.run(host="0.0.0.0", port=80)
+@app.route("/db")
+def get_db():
+    if db.is_connected():
+        return "Successfully connected to MySQL database"
+    else:
+        return "There is a problem with the MySQL connection"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
